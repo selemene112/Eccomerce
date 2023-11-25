ebefaf0f34d0f7c9f71f900497a92221dd0388fe
selemene112 <ariyanda98y@gmail.com>
2023-11-25 17:26:02 +0800

    Add v1.1.1 Add Product models

---
+++ Models/Product.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	UserID uuid.UUID `gorm:"type:uuid;not null"`
+	Name   string
+	Price  float64
+	gorm.Model
+}
