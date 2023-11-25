fba3f2aa7eab488c9cf33c4ee91c1f5933f9dcc1
selemene112 <ariyanda98y@gmail.com>
2023-11-25 17:29:08 +0800

    Add v1.1.1 Add Users Models

--- Models/Users.go
+++ Models/Users.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID       uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name     string    `gorm:"type:varchar(255);not null"`
+	Email    string    `gorm:"type:varchar(255);not null;unique"`
+	Password string    `gorm:"type:varchar(255);not null"`
+	Wallet   Wallet
+	Products []Product
+	Topup    []Topup
+	Payment  []Payment
+}
