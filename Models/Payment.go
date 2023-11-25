baf1948280135a43bd1d67009f0f0c70a7bc492d
selemene112 <ariyanda98y@gmail.com>
2023-11-25 17:24:59 +0800

    Add v1.1.1  Add Payment Models

---
+++ Models/Payment.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Payment struct {
+	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	UserID         uuid.UUID `gorm:"type:uuid;not null"`
+	WalletID       uuid.UUID `gorm:"type:uuid;not null"`
+	PreviousWallet float64
+	AmountPayment  float64
+	CurrentWallet  float64
+	CreatedAt      time.Time
+}
