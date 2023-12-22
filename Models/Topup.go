8185f0e53c94c9ddbdb084c1c06e7f6f5ee72a4c
selemene112 <ariyanda98y@gmail.com>
2023-12-22 09:53:49 +0800

    Add Models for top up

--- Models/Topup.go
+++ Models/Topup.go
@@ -9,7 +9,7 @@ import (
 type Topup struct {
 	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	UserID         uuid.UUID `gorm:"type:uuid;not null"`
-	WalletID       uuid.UUID `gorm:"type:uuid;not null"`
+	WalletID       uuid.UUID `gorm:"type:uuid;not null;foreignKey:ID"`
 	PreviousWallet float64
 	AmountTopup    float64
 	CurrentWallet  float64
